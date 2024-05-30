fac1211a749a471de1581250e4163e64213cbbe8
Ehren Abbott <era12358@gmail.com>
2024-05-30 15:22:14 -0500

    Removed superfluouspages, components and assets that come with new react project

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Kommr</h1>
+      <p>Hi</p>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
