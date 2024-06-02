ca6a16bde5dd160e5eb31ae9a771924c8560e0a4
Ehren Abbott <era12358@gmail.com>
2024-06-02 13:36:31 -0500

    Added apple, fb, and goole login buttons and styling

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,6 @@
-import { firebaseConfig } from '../config/firebase.config.js'
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
+// import { firebaseConfig } from '../config/firebase.config.js'
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
 
+// export const auth = getAuth(app)
+// export const app = initializeApp(firebaseConfig)
--- src/login.jsx
+++ src/login.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { firebaseConfig } from '../config/firebase.config.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
 
+
+/* === Firebase Setup === */
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+/* == UI - Elements == */
+const emailInputEl = document.getElementById('login-input')
+console.log(emailInputEl)
+
+function authCreateAccountWithEmail() { 
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    }
 
 export default function Login(){ 
     return ( 
         <form className='login-container'> 
             <h1 className='login-title'>Kommr</h1>
             <div className='login-input'>
-                <input placeholder="Email"></input>
-                <input placeholder="Password"></input>
+                <input id='email-input' placeholder="Email"></input>
+                <input id='password-input' placeholder="Password"></input>
             </div>
             <button className='login-button'>Login</button>
             <button className='create-account-button'>Create Account</button>
+            <p className='alt-login-text'>-or login with-</p>
+            <div className='login-logo-container'> 
+                <button className='apple-btn'><img src="../assets/icons/apple.png"></img></button>
+                <button className='fb-btn'><img src="../assets/icons/facebook.png"></img></button>
+                <button className='google-btn'><img src="../assets/icons/google.png"></img></button>
+            </div>
         </form>
     )
 }
