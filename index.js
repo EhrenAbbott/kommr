9f1a5f8955c65ace5a5fe12b8aff0d91a5feb75c
Ehren Abbott <era12358@gmail.com>
2024-06-08 12:43:32 -0500

    Enabled account creating via email with authCreateAccountWithEmail

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const loginBtnEl = document.getElementById("login-button")
+//Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
+import { firebaseConfig } from '../config/firebase.config.js'
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
 
-loginBtnEl.addEventListener("click", works)
 
-function works(){ 
-    console.log("works")
-}
+//Global Variables
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+//HTML Elements//
+const createAccountButtonEl = document.getElementById("create-account-btn")
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+//Event Listeners
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+
+
+function authCreateAccountWithEmail() { 
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential)
+        })
+        .catch((error) => {
+            console.error(error.message)
+        });
+    }
+
+    // function authCreateAccountWithEmail() {
+    //     const email = emailInputEl.value
+    //     const password = passwordInputEl.value
+    
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then((userCredential) => {
+    //             console.log(userCredential)
+    //         })
+    //         .catch((error) => {
+    //             console.error(error.message) 
+    //         })
+    // }
+
