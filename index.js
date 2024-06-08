8c1611c31749b00f337183a53c28672869e0290a
Ehren Abbott <era12358@gmail.com>
2024-06-08 12:55:37 -0500

    Added authSignInWithEmail

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/fireba
 import { firebaseConfig } from '../config/firebase.config.js'
 import { 
     getAuth, 
-    createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
 
 
 //Global Variables
@@ -14,11 +15,25 @@ const auth = getAuth(app)
 const createAccountButtonEl = document.getElementById("create-account-btn")
 const emailInputEl = document.getElementById("email-input")
 const passwordInputEl = document.getElementById("password-input")
+const loginBtnEl = document.getElementById("login-btn")
 
 //Event Listeners
 createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+loginBtnEl.addEventListener("click", authSignInWithEmail)
 
-
+function authSignInWithEmail() { 
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("successfully logged in")
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage)
+        });
+    }
 
 function authCreateAccountWithEmail() { 
     const email = emailInputEl.value
@@ -26,23 +41,11 @@ function authCreateAccountWithEmail() {
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            console.log(userCredential)
+            console.log("account created with email")
         })
         .catch((error) => {
             console.error(error.message)
         });
     }
 
-    // function authCreateAccountWithEmail() {
-    //     const email = emailInputEl.value
-    //     const password = passwordInputEl.value
-    
-    //     createUserWithEmailAndPassword(auth, email, password)
-    //         .then((userCredential) => {
-    //             console.log(userCredential)
-    //         })
-    //         .catch((error) => {
-    //             console.error(error.message) 
-    //         })
-    // }
 
